3069b8b12559e290026908b93499ad061f78feca
Jon Shamir <jon.shamir@gmail.com>
2023-07-27 20:46:25 +0300

    Add react-router basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   // <React.StrictMode>
-  <Router>
+  <Router basename="/herbs/">
     <App />
   </Router>,
   // </React.StrictMode>,
